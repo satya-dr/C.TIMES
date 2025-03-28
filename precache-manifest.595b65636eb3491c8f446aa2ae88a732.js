98f368b0d6a09f4652bbc53b72948d951f9c0067
SATYAJIT ROY <satyajitroy19599@gmail.com>
2025-03-28 21:11:10 +0530

    Updates

---
+++ precache-manifest.595b65636eb3491c8f446aa2ae88a732.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4f25c91acc01ef268c21cbb6ab742f",
+    "url": "/C.TIMES/index.html"
+  },
+  {
+    "revision": "3c6ce8004b0fee444849",
+    "url": "/C.TIMES/static/css/main.e6c40bc3.chunk.css"
+  },
+  {
+    "revision": "61bea11a46a59577cb0c",
+    "url": "/C.TIMES/static/js/2.0a151e15.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/C.TIMES/static/js/2.0a151e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fbb30a346d5606348e",
+    "url": "/C.TIMES/static/js/3.10bb59d0.chunk.js"
+  },
+  {
+    "revision": "3c6ce8004b0fee444849",
+    "url": "/C.TIMES/static/js/main.66f64fdc.chunk.js"
+  },
+  {
+    "revision": "36a694afac6a31edfab4",
+    "url": "/C.TIMES/static/js/runtime-main.5f2ddf3a.js"
+  },
+  {
+    "revision": "a40fdbf0f2719211bf2fe1c38df80af5",
+    "url": "/C.TIMES/static/media/NO1.a40fdbf0.jpg"
+  },
+  {
+    "revision": "0bc1e549f0daba076bdfa2afb1d9be5c",
+    "url": "/C.TIMES/static/media/NO2.0bc1e549.jpg"
+  },
+  {
+    "revision": "c1fc0fe560a39024def57d992db73fce",
+    "url": "/C.TIMES/static/media/NO3.c1fc0fe5.jpg"
+  },
+  {
+    "revision": "d0e2ba2ab796484e8fc03f9db8771d6d",
+    "url": "/C.TIMES/static/media/art1.d0e2ba2a.jpg"
+  },
+  {
+    "revision": "588aacb2b466a1d2f58a961b63ac37c6",
+    "url": "/C.TIMES/static/media/art2.588aacb2.jpg"
+  },
+  {
+    "revision": "c05b5d1bf9488b7323094a2e2dd1703b",
+    "url": "/C.TIMES/static/media/art3.c05b5d1b.jpg"
+  },
+  {
+    "revision": "005ad1727ac5e63cee93253502a89718",
+    "url": "/C.TIMES/static/media/art4.005ad172.jpg"
+  },
+  {
+    "revision": "b395d33e461c917d25be5e2005a4fad7",
+    "url": "/C.TIMES/static/media/art5.b395d33e.jpg"
+  },
+  {
+    "revision": "65b2b8cb1340fc1003f3e943cc93438b",
+    "url": "/C.TIMES/static/media/art6.65b2b8cb.jpg"
+  },
+  {
+    "revision": "1f7369cea36ebea60391c8a048b0f8b1",
+    "url": "/C.TIMES/static/media/cgec_acdemic.1f7369ce.jpeg"
+  },
+  {
+    "revision": "e394d69733c9038f9d4883074d1503f3",
+    "url": "/C.TIMES/static/media/cgectimes.e394d697.png"
+  },
+  {
+    "revision": "a3fdaa3924672ac3678c39ee6f203bc5",
+    "url": "/C.TIMES/static/media/clgLogo.a3fdaa39.png"
+  },
+  {
+    "revision": "607b9f69862d76af04b474113c0c7ff5",
+    "url": "/C.TIMES/static/media/india.607b9f69.png"
+  }
+]);
